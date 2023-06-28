7b18cdfa3be36dc4dcc47c8bcdac60b7f4f574bf
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2023-06-28 21:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function threeSum(arr, target) {
-// write your code here
-  
+//your code here
+function decimalToBinary(num){
+    let binaryStr = ""
+
+    while(num>0){
+
+        let rem = num%2;
+
+        // Method 1 - Appending the new binary character to the front of the string
+        // binaryStr = rem.toString() + binaryStr
+
+        // Method 2 - Append it to the end
+        binaryStr = binaryStr + rem.toString()
+
+        num = Math.floor(num/2)
+    }
+    // Method - 1
+    // return binaryStr
+
+
+    // Method 2- Reverse it
+    return binaryStr.split("").reverse().join("")
 }
 
-module.exports = threeSum;
+module.exports = decimalToBinary;
